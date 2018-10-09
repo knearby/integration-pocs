1f94fb2ee8a5f127be23ba946b8d442570ad3071
zephinzer <gitlab@joeir.net>
2018-10-09 09:52:23 +0800

    completed get nearby places

--- apis/google/get-places.js
+++ apis/google/get-places.js
@@ -1,19 +1,21 @@
-const {getData, getEnvValue} = require('../utils');
+const { getData, getEnvValue } = require('../utils');
 
 // query options and defaults
-const REQ_LAT = process.env.REQ_LAT || 1.3521;
-const REQ_LON = process.env.REQ_LON || 103.8198;
+const REQ_LAT = process.env.REQ_LAT || 1.2998;
+const REQ_LON = process.env.REQ_LON || 103.7876;
+const REQ_RAD = process.env.REQ_RAD || 500;
 const REQ_TEXT = process.env.REQ_TEXT || 'cafe';
 
 // API administration stuff
-const API_KEY = getEnvValue('GOOGLE_API_KEY');
+const API_KEY = getEnvValue('GOOGLE_PLACES_API_KEY');
 const REQ_HOST = 'maps.googleapis.com';
 const REQ_METHOD = 'GET';
-const REQ_PATH = '/maps/api/place/findplacefromtext/json' +
-  `?&key=${API_KEY}` +
-  `&input=${REQ_TEXT}` +
-  `&locationbias=point:${REQ_LAT},${REQ_LON}`
-;
+const REQ_PATH =
+  '/maps/api/place/nearbysearch/json' +
+  `?key=${API_KEY}` +
+  `&keyword=${REQ_TEXT}` +
+  `&location=${REQ_LAT},${REQ_LON}` +
+  `&radius=${REQ_RAD}`;
 
 getData({
   id: 'get-places',
@@ -21,7 +23,7 @@ getData({
   method: REQ_METHOD,
   platform: 'google',
   useCache: true,
-  uriPath: REQ_PATH,
-}).then((data) => {
+  uriPath: REQ_PATH
+}).then(data => {
   console.info(data);
 });
