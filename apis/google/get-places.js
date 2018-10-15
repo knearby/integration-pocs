58e7c17da0381222e5f2a7981ac253719a5da84b
zephinzer <dev@joeir.net>
2018-10-15 22:22:01 +0800

    updated returned information

--- apis/google/get-places.js
+++ apis/google/get-places.js
@@ -1,29 +1,42 @@
 const { getData, getEnvValue } = require('../utils');
 
+const POC_ID = 'get-places';
+
 // query options and defaults
 const REQ_LAT = process.env.REQ_LAT || 1.2998;
 const REQ_LON = process.env.REQ_LON || 103.7876;
-const REQ_RAD = process.env.REQ_RAD || 500;
-const REQ_TEXT = process.env.REQ_TEXT || 'cafe';
+const REQ_RAD = process.env.REQ_RAD || 2000;
+const REQ_TEXT = process.env.REQ_TEXT || 'food';
 
 // API administration stuff
 const API_KEY = getEnvValue('GOOGLE_PLACES_API_KEY');
 const REQ_HOST = 'maps.googleapis.com';
 const REQ_METHOD = 'GET';
 const REQ_PATH =
-  '/maps/api/place/nearbysearch/json' +
-  `?key=${API_KEY}` +
-  `&keyword=${REQ_TEXT}` +
-  `&location=${REQ_LAT},${REQ_LON}` +
-  `&radius=${REQ_RAD}`;
+  '/maps/api/place/nearbysearch/json?' +
+  [
+    `key=${API_KEY}`,
+    `keyword=${REQ_TEXT}`,
+    `location=${REQ_LAT},${REQ_LON}`,
+    `radius=${REQ_RAD}`,
+  ].join('&');
 
 getData({
-  id: 'get-places',
+  id: POC_ID,
   hostname: REQ_HOST,
   method: REQ_METHOD,
   platform: 'google',
   useCache: true,
   uriPath: REQ_PATH
 }).then(data => {
-  console.info(data.results);
+  console.info(data['next_page_token']);
+  console.info(data.results.map((result) => ({
+    lat: result.geometry.location.lat,
+    lng: result.geometry.location.lng,
+    placeId: result.place_id,
+    name: result.name,
+    rating: result.rating ? result.rating : null,
+    types: result.types,
+    address: result.vicinity ? result.vicinity : null,
+  })));
 });
